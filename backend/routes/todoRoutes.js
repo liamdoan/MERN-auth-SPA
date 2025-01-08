930c31d902f7b15533019b83ac59413c73580d90
liamdoan <lamdoan9898@gmail.com>
2025-01-08 11:44:24 +0200

    add cmt

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/verifyToken');
 const { getTodos, createTodos, updateToDo, toggleCompleted, deleteToDo } = require('../controllers/todo-controllers/todoController');
 
+//verify user token first before carrying on with CRUD functions
 router.get("/get", verifyToken, getTodos);
 router.post("/save", verifyToken, createTodos);
 router.put("/update/:id", verifyToken, updateToDo);
