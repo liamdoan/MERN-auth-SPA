a872386f5512a27770fc0fe44f175c88b30f3d8f
liamdoan <lamdoan9898@gmail.com>
2025-01-08 10:26:38 +0200

    create controllers for todo task

---
+++ backend/controllers/todo-controllers/todoController.js
@@ -0,0 +1,41 @@
+const todoModel = require('../../models/todoModel');
+
+module.exports.getTodos = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+module.exports.createTodos = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+module.exports.updateToDo = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+module.exports.toggleCompleted = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+module.exports.deleteToDo = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
