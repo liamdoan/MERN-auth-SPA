2eaaa70776348dc82d841fea21d8538ea50fe3ea
liamdoan <lamdoan9898@gmail.com>
2025-01-08 15:42:39 +0200

    create a separate file to store baseUrl

---
+++ client/src/utils/baseUrl.ts
@@ -0,0 +1,5 @@
+// for local test and development
+export const baseUrl = 'http://localhost:5000/api/todo';
+
+// for after deploying to server, use this url instead
+// export const baseUrl = 'https://find-out-later/api/todo';
