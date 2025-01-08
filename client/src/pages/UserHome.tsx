6ef184363e07ddfa9c7df9427043d2fd56246749
liamdoan <lamdoan9898@gmail.com>
2025-01-08 22:14:18 +0200

    add type to user obj

--- client/src/pages/UserHome.tsx
+++ client/src/pages/UserHome.tsx
@@ -8,13 +8,14 @@ import LoadingBar from "../components/loading/LoadingBar";
 import Spinner from "../components/loading/Spinner";
 import { userLogout } from "../utils/authApiCalls";
 import Todo from "../components/main-app/Todo";
+import { UserType } from "../utils/types";
 
 const UserHome = () => {
     const [loading, setLoading] = useState<Boolean>(false);
     const [messageSuccess, setMessageSuccess] = useState<string>("");
     const [messageFailed, setMessageFailed] = useState<string>("");
 
-    const user: any = useSelector((state: RootState) => state.user.user);
+    const user = useSelector((state: RootState) => state.user.user) as UserType | null;
     const isCheckingUserAuthenticated = useSelector((state: RootState) => state.user.isCheckingUserAuthenticated);
     const isUserAuthenticated = useSelector((state: RootState) => state.user.isUserAuthenticated);
     const dispatch = useDispatch();
@@ -49,10 +50,10 @@ const UserHome = () => {
         <div className={styles.wrapper}>
             <div className={styles.navbar}>
                 <div className={styles.userInfo}>
-                    <p>Hello, {user.name}</p>
-                    <p>Your email is: {user.email}</p>
-                    <p>You joined on: {new Date (user.createdAt).toLocaleString()}</p>
-                    <p>You last logged in on: {user && new Date(user.lastLogin).toLocaleString()}</p>
+                    <p>Hello, {user?.name}</p>
+                    <p>Your email is: {user?.email}</p>
+                    <p>You joined on: {new Date (user?.createdAt as string).toLocaleString()}</p>
+                    <p>You last logged in on: {user && new Date(user?.lastLogin).toLocaleString()}</p>
                 </div>
                 <button className={styles.logoutButton} onClick={handleLogout}>Log Out</button>
             </div>
