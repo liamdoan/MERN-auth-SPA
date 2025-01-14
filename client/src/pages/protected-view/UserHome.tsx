1780bb73c66219dc638e6afb128e1f4dd6298917
liamdoan <lamdoan9898@gmail.com>
2025-01-14 22:44:47 +0200

    Show roles in userHome info

--- client/src/pages/protected-view/UserHome.tsx
+++ client/src/pages/protected-view/UserHome.tsx
@@ -52,6 +52,7 @@ const UserHome = () => {
                 <div className={styles.userInfo}>
                     <p>Hello, {user?.name}</p>
                     <p>Your email is: {user?.email}</p>
+                    <p>Your role is: {user?.roles.join(', ')}</p>
                     <p>You joined on: {new Date (user?.createdAt as string).toLocaleString()}</p>
                     <p>You last logged in on: {user && new Date(user?.lastLogin).toLocaleString()}</p>
                 </div>
