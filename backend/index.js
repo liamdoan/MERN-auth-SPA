67efa582e345e0dae6ce136dae50b3ca2ad87e89
liamdoan <lamdoan9898@gmail.com>
2025-01-15 06:29:48 +0200

    test cors

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors({
-//     origin: process.env.DEVELOPMENT_CLIENT_URL,
-//     credentials: true
-// }));
-app.use(cors());
+app.use(cors({
+    origin:"https://mern-auth-spa-client.vercel.app",
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
