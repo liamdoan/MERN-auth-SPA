e71bdc5c0846a84c4f151d95ac92e5f6d9bfba61
liamdoan <lamdoan9898@gmail.com>
2025-01-15 05:43:41 +0200

    change value of origin to dynamic value

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.DEVELOPMENT_CLIENT_URL,
     credentials: true
 }));
 app.use(express.json());
