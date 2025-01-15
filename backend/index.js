3d5ca742329afae53ef291a263eeb2454233c2c0
liamdoan <lamdoan9898@gmail.com>
2025-01-15 06:47:14 +0200

    test cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.DEVELOPMENT_CLIENT_URL);
+    res.setHeader('Access-Control-Allow-Origin', 'https://mern-auth-spa-client.vercel.app');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
