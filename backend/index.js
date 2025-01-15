8111df4e0d56a319ef1b13ce5366b3ff564d842e
liamdoan <lamdoan9898@gmail.com>
2025-01-15 07:52:19 +0200

    test cors 7

--- backend/index.js
+++ backend/index.js
@@ -10,27 +10,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: process.env.DEVELOPMENT_CLIENT_URL,
+    credentials: true
 }));
-
-console.log("from server:", process.env.DEVELOPMENT_CLIENT_URL)
-
 app.use(express.json());
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mern-auth-spa-client.vercel.app');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(204).end();
-    }
-    next();
-});
 
 const authRoutes = require("./routes/auth.js");
 const todoRoutes = require("./routes/todoRoutes.js");
