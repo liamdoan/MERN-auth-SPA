80292cd05902cddd846a81ecc34c2d464995a1cd
Lam Doan <lam.doan@dreambroker.fi>
2025-02-05 15:44:53 +0200

    fix path

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/todo', todoRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/dist")));
+	app.use(express.static(path.join(__dirname, "/client")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "index.html"));
 	});
 }
 
